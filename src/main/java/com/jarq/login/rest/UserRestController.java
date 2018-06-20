b8e82beb1e8a73637742edecf72f9ec8d7290ee4
Jarosław <jarek.kow@gmail.com>
2018-06-20 15:23:34 +0200

    add POST/PUT/DELETE to User REST API

--- src/main/java/com/jarq/login/rest/UserRestController.java
+++ src/main/java/com/jarq/login/rest/UserRestController.java
@@ -3,10 +3,7 @@ package com.jarq.login.rest;
 import com.jarq.login.entity.User;
 import com.jarq.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,9 +29,41 @@ public class UserRestController {
         if (theUser == null) {
             throw new UserNotFoundException("User id not found - " + userId);
         }
+        return theUser;
+    }
+
+    // add mapping for POST /users - add new user
+    @PostMapping("/users")
+    public User addUser(@RequestBody User theUser) {
+
+        // set id to 0 - is force a save new item .. instead update
+        theUser.setId(0);
+
+        userService.saveUser(theUser);
 
         return theUser;
     }
 
+    // add mapping for PUT /users - update existing user
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User theUser) {
 
+        userService.saveUser(theUser);
+        return theUser;
+    }
+
+    // add mapping for DELETE /users/{userId} - delete user
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable int userId) {
+
+        User tempUser = userService.getUser(userId);
+
+        if (tempUser == null) {
+            throw new UserNotFoundException("User id not found - " + userId);
+        }
+
+        userService.deleteUser(userId);
+
+        return "Deleted user id - " + userId;
+    }
 }
